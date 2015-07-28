cee45eb43ad1374c3bbbc65b7871180195bc0109
James Fisher <fisher.jc@gmail.com>
2015-07-28 19:55:27 -0400

    add delete to wiki page

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -13,6 +13,7 @@ class WikisController < ApplicationController
 
   def create
     @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+    
     if @wiki.save
       flash[:notice] = "Wiki article was saved."
       redirect_to @wiki
@@ -28,6 +29,7 @@ class WikisController < ApplicationController
 
   def update
     @wiki = wiki.find(params[:id])
+    
     if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
      flash[:notice] = "Wiki article was updated."
      redirect_to @wiki
@@ -35,5 +37,17 @@ class WikisController < ApplicationController
      flash[:error] = "There was an error saving the wiki. Please try again."
      render :edit
     end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+    
+    if @wiki.destroy
+     flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+     redirect_to @wiki
+    else
+     flash[:error] = "There was an error deleting the wiki."
+     render :show
+   end
   end
 end
