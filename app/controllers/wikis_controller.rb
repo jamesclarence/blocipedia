5e842aeb8309ed92c54e03eca21425a69631b3d5
James Fisher <fisher.jc@gmail.com>
2015-07-28 19:31:41 -0400

    add views to wiki edit, new, and show

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -8,8 +8,32 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new(params.require(:wiki).permit(:title, :body))
+    if @wiki.save
+      flash[:notice] = "Wiki article was saved."
+      redirect_to @wiki
+    else
+      flash[:error] = "There was an error saving the wiki. Please try again."
+      render :new
+    end
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
+  end
+
+  def update
+    @wiki = wiki.find(params[:id])
+    if @wiki.update_attributes(params.require(:wiki).permit(:title, :body))
+     flash[:notice] = "Wiki article was updated."
+     redirect_to @wiki
+    else
+     flash[:error] = "There was an error saving the wiki. Please try again."
+     render :edit
+    end
   end
 end
